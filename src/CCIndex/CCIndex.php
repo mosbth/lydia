c26a73cab2d66cf350b47abdebdb7a62ed2a0314
Mikael Roos <mikael.t.h.roos@gmail.com>
2012-02-14 15:43:53 +0100

    Adding a menu to the index controller.

--- src/CCIndex/CCIndex.php
+++ src/CCIndex/CCIndex.php
@@ -10,9 +10,30 @@ class CCIndex implements IController {
  	 * Implementing interface IController. All controllers must have an index action.
 	 */
 	public function Index() {	
-		global $ly;
-		$ly->data['title'] = "The Index Controller";
-		$ly->data['main'] = "<h1>The Index Controller</h1>";
+    $this->Menu();
 	}
 
+
+	/**
+ 	 * Create a method that shows the menu, same for all methods
+	 */
+	private function Menu() {	
+		$ly = CLydia::Instance();
+		$menu = array('index', 'index/index', 'developer', 'developer/index', 'developer/links');
+		
+		$html = null;
+		foreach($menu as $val) {
+		  $html .= "<li><a href='" . $ly->request->CreateUrl($val) . "'>$val</a>";  
+		}
+		
+		$ly->data['title'] = "The Index Controller";
+		$ly->data['main'] = <<<EOD
+<h1>The Index Controller</h1>
+<p>This is what you can do for now:</p>
+<ul>
+$html
+</ul>
+EOD;
+  }
+  
 } 
