0b8abd467001107da4adc24c1c032e8e9529905c
Mikael Roos <mikael.t.h.roos@gmail.com>
2012-02-24 16:47:17 +0100

    Added GetIncludeContents which includes a file and returns the result as a string

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -24,10 +24,24 @@ spl_autoload_register('autoload');
 /**
  * Set a default exception handler and enable logging in it.
  */
-function exception_handler($e) {
+function exceptionHandler($e) {
   echo "Lydia: Uncaught exception: <p>" . $e->getMessage() . "</p><pre>" . $e->getTraceAsString(), "</pre>";
 }
-set_exception_handler('exception_handler');
+set_exception_handler('exceptionHandler');
+
+
+/**
+ * Helper, include a file and store it in a string. Make $vars available to the included file.
+ */
+function getIncludeContents($filename, $vars=array()) {
+  if (is_file($filename)) {
+    ob_start();
+    extract($vars);
+    include $filename;
+    return ob_get_clean();
+  }
+  return false;
+}
 
 
 /**
