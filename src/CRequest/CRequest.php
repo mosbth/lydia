62c9697c34654456e4923ab084875d68e7c2881c
Mikael Roos <mikael.t.h.roos@gmail.com>
2012-03-21 18:06:54 +0100

    Corrected handling of index.php/q=controller/metod and index.php/controller/metod

--- src/CRequest/CRequest.php
+++ src/CRequest/CRequest.php
@@ -66,53 +66,58 @@ class CRequest {
   }
 
 
-	/**
-	 * Init the object by parsing the current url request.
-	 */
+  /**
+   * Parse the current url request and divide it in controller, method and arguments.
+   *
+   * Calculates the base_url of the installation. Stores all useful details in $this.
+   *
+   * @param $baseUrl string use this as a hardcoded baseurl.
+   */
   public function Init($baseUrl = null) {
-		// Take current url and divide it in controller, method and arguments
-		$requestUri = $_SERVER['REQUEST_URI'];
-		$scriptPart = $scriptName = $_SERVER['SCRIPT_NAME'];    
-
-		// Check if url is in format controller/method/arg1/arg2/arg3
-		if(substr_compare($requestUri, $scriptName, 0)) {
-	    $scriptPart = dirname($scriptName);
-		}
-
-		// Set query to be everything after base_url, except the optional querystring
-		$query = trim(substr($requestUri, strlen(rtrim($scriptPart, '/'))), '/');
-		$pos = strcspn($query, '?');
-    if($pos) {
-      $query = substr($query, 0, $pos);    
+    $requestUri = $_SERVER['REQUEST_URI'];
+    $scriptName = $_SERVER['SCRIPT_NAME'];    
+    
+    // Compare REQUEST_URI and SCRIPT_NAME as long they match, leave the rest as current request.
+    $i=0;
+    $len = min(strlen($requestUri), strlen($scriptName));
+    while($i<$len && $requestUri[$i] == $scriptName[$i]) {
+      $i++;
+    }
+    $request = trim(substr($requestUri, $i), '/');
+  
+    // Remove the ?-part from the query when analysing controller/metod/arg1/arg2
+    $queryPos = strpos($request, '?');
+    if($queryPos !== false) {
+      $request = substr($request, 0, $queryPos);
     }
     
-		// Check if this looks like a querystring approach link
-    if(substr($query, 0, 1) === '?' && isset($_GET['q'])) {
-      $query = trim($_GET['q']);
+    // Check if request is empty and querystring link is set
+    if(empty($request) && isset($_GET['q'])) {
+      $request = trim($_GET['q']);
     }
-		$splits = explode('/', $query);
-		
-		// Set controller, method and arguments
-		$controller =  !empty($splits[0]) ? $splits[0] : 'index';
-		$method 		=  !empty($splits[1]) ? $splits[1] : 'index';
-		$arguments = $splits;
-		unset($arguments[0], $arguments[1]); // remove controller & method part from argument list
-		
-		// Prepare to create current_url and base_url
-		$currentUrl = $this->GetCurrentUrl();
-		$parts 	    = parse_url($currentUrl);
-		$baseUrl 		= !empty($baseUrl) ? $baseUrl : "{$parts['scheme']}://{$parts['host']}" . (isset($parts['port']) ? ":{$parts['port']}" : '') . rtrim(dirname($scriptName), '/');
-		
-		// Store it
-		$this->base_url 	  = rtrim($baseUrl, '/') . '/';
-		$this->current_url  = $currentUrl;
-		$this->request_uri  = $requestUri;
-		$this->script_name  = $scriptName;
-		$this->query	      = $query;
-		$this->splits	      = $splits;
-		$this->controller	  = $controller;
-		$this->method	      = $method;
-		$this->arguments    = $arguments;
+    $splits = explode('/', $request);
+    
+    // Set controller, method and arguments
+    $controller =  !empty($splits[0]) ? $splits[0] : 'index';
+    $method 		=  !empty($splits[1]) ? $splits[1] : 'index';
+    $arguments = $splits;
+    unset($arguments[0], $arguments[1]); // remove controller & method part from argument list
+    
+    // Prepare to create current_url and base_url
+    $currentUrl = $this->GetCurrentUrl();
+    $parts 	    = parse_url($currentUrl);
+    $baseUrl 		= !empty($baseUrl) ? $baseUrl : "{$parts['scheme']}://{$parts['host']}" . (isset($parts['port']) ? ":{$parts['port']}" : '') . rtrim(dirname($scriptName), '/');
+    
+    // Store it
+    $this->base_url 	  = rtrim($baseUrl, '/') . '/';
+    $this->current_url  = $currentUrl;
+    $this->request_uri  = $requestUri;
+    $this->script_name  = $scriptName;
+    $this->request      = $request;
+    $this->splits	      = $splits;
+    $this->controller	  = $controller;
+    $this->method	      = $method;
+    $this->arguments    = $arguments;
   }
 
 
