96540a960c70707978e0d573d2fcc160e4371b03
Mikael Roos <mikael.t.h.roos@gmail.com>
2012-04-13 10:34:38 +0200

    all methods are now protected again

--- src/CObject/CObject.php
+++ src/CObject/CObject.php
@@ -90,7 +90,7 @@ class CObject {
    * @param $message string the message.
    * @param $alternative string the message if the $type is set to false, defaults to null.
    */
-  public function AddMessage($type, $message, $alternative=null) {
+  protected function AddMessage($type, $message, $alternative=null) {
     if($type === false) {
       $type = 'error';
       $message = $alternative;
