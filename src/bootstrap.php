da4c12c88d085c79f88d05252628b5c4b5034425
Mikael Roos <mikael.t.h.roos@gmail.com>
2012-04-13 10:31:21 +0200

    added function for time formatting since

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -50,3 +50,94 @@ function getIncludeContents($filename, $vars=array()) {
 function htmlent($str, $flags = ENT_COMPAT) {
   return htmlentities($str, $flags, CLydia::Instance()->config['character_encoding']);
 }
+
+
+/**
+ * Helper, interval formatting of times. Needs PHP5.3. 
+ *
+ * All times in database is UTC so this function assumes the starttime to be in UTC, if not otherwise
+ * stated.
+ *
+ * Copied from http://php.net/manual/en/dateinterval.format.php#96768
+ * Modified (mos) to use timezones.
+ * A sweet interval formatting, will use the two biggest interval parts.
+ * On small intervals, you get minutes and seconds.
+ * On big intervals, you get months and days.
+ * Only the two biggest parts are used.
+ *
+ * @param DateTime|string $start
+ * @param DateTimeZone|string|null $startTimeZone
+ * @param DateTime|string|null $end
+ * @param DateTimeZone|string|null $endTimeZone
+ * @return string
+ */
+function formatDateTimeDiff($start, $startTimeZone=null, $end=null, $endTimeZone=null) {
+  if(!($start instanceof DateTime)) {
+    if($startTimeZone instanceof DateTimeZone) {
+      $start = new DateTime($start, $startTimeZone);
+    } else if(is_null($startTimeZone)) {
+      $start = new DateTime($start);
+    } else {
+      $start = new DateTime($start, new DateTimeZone($startTimeZone));
+    }
+  }
+  
+  if($end === null) {
+    $end = new DateTime();
+  }
+  
+  if(!($end instanceof DateTime)) {
+    if($endTimeZone instanceof DateTimeZone) {
+      $end = new DateTime($end, $endTimeZone);
+    } else if(is_null($endTimeZone)) {
+      $end = new DateTime($end);
+    } else {
+      $end = new DateTime($end, new DateTimeZone($endTimeZone));
+    }
+  }
+  
+  $interval = $end->diff($start);
+  $doPlural = function($nb,$str){return $nb>1?$str.'s':$str;}; // adds plurals
+  //$doPlural = create_function('$nb,$str', 'return $nb>1?$str."s":$str;'); // adds plurals
+  
+  $format = array();
+  if($interval->y !== 0) {
+    $format[] = "%y ".$doPlural($interval->y, "year");
+  }
+  if($interval->m !== 0) {
+    $format[] = "%m ".$doPlural($interval->m, "month");
+  }
+  if($interval->d !== 0) {
+    $format[] = "%d ".$doPlural($interval->d, "day");
+  }
+  if($interval->h !== 0) {
+    $format[] = "%h ".$doPlural($interval->h, "hour");
+  }
+  if($interval->i !== 0) {
+    $format[] = "%i ".$doPlural($interval->i, "minute");
+  }
+  if(!count($format)) {
+      return "less than a minute";
+  }
+  if($interval->s !== 0) {
+    $format[] = "%s ".$doPlural($interval->s, "second");
+  }
+  
+  if($interval->s !== 0) {
+      if(!count($format)) {
+          return "less than a minute";
+      } else {
+          $format[] = "%s ".$doPlural($interval->s, "second");
+      }
+  }
+  
+  // We use the two biggest parts
+  if(count($format) > 1) {
+      $format = array_shift($format)." and ".array_shift($format);
+  } else {
+      $format = array_pop($format);
+  }
+  
+  // Prepend 'since ' or whatever you like
+  return $interval->format($format);
+}
