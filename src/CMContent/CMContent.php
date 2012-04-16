2ee57ae516daf84effeae4777cadf5011a095f3e
Mikael Roos <mikael.t.h.roos@gmail.com>
2012-04-16 16:52:39 +0200

    Added bbcode formatting capability

--- src/CMContent/CMContent.php
+++ src/CMContent/CMContent.php
@@ -155,7 +155,7 @@ class CMContent extends CObject implements IHasSQL, ArrayAccess {
     switch($filter) {
       /*case 'php': $data = nl2br(makeClickable(eval('?>'.$data))); break;
       case 'html': $data = nl2br(makeClickable($data)); break;*/
-      case 'bbcode': $data = nl2br(bbcode2html($data)); break;
+      case 'bbcode': $data = nl2br(bbcode2html(htmlEnt($data))); break;
       case 'plain': 
       default: $data = nl2br(makeClickable(htmlEnt($data))); break;
     }
