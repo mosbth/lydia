77ee1fcc975389e23e3d860c373b864e4bcc6a97
Mikael Roos <mikael.t.h.roos@gmail.com>
2012-08-22 12:26:55 +0200

    Adding missing </div>

--- views/CCBlog/index_sidebar.tpl.php
+++ views/CCBlog/index_sidebar.tpl.php
@@ -1,13 +1,14 @@
 <?php if($contents != null):?>
 
 <div class='box'>
-<h4>Senaste inläggen</h4>
-<ul>
-
-<?php foreach($contents as $val):?>
-<li><a href='<?=create_url(null, $val['key'])?>'><?=esc($val['title'])?></a></li>
-<?php endforeach; ?>
-</ul>
+  <h4>Senaste inläggen</h4>
+  <ul>
+  
+  <?php foreach($contents as $val):?>
+  <li><a href='<?=create_url(null, $val['key'])?>'><?=esc($val['title'])?></a></li>
+  <?php endforeach; ?>
+  </ul>
+</div>
 
 <?php else:?>
 <p><?=t('No posts exists.')?></p>
